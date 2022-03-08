1ab3e57e2759486ac182e489b733ef6b3f55833a
Manish Bhardwaj <manish.bhardwaj@nagarro.com>
2022-03-08 16:38:28 +0530

    jhjkh

--- ClassLibrary1/Controllers/WeatherForecastController.cs
+++ ClassLibrary1/Controllers/WeatherForecastController.cs
@@ -31,8 +31,8 @@ namespace WebApplication1.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = rng.Next(-20, 55)
+                //Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
         }
